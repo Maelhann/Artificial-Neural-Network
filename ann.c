d6999d231a6aaa35addfdf3cf86bc2f1949ecb06
Maelhann Roze <ankou@roaming-54-ee-75-d1-cb-78.ic.ac.uk>
2018-08-01 19:46:44 -0500

    can now free an alloc'd ANN

--- ann.c
+++ ann.c
@@ -9,35 +9,36 @@ ann_t *ann_create(int num_layers, int *layer_outputs)
   int i =  2 ;
   layer_t* prev = network->input_layer ; 
   layer_t* curr = layer_create() ; 
-  while(i < num_layers){	  
-   layer_init(curr,layer_outputs[i],prev);
-   prev->next = curr; 
-   if(i == num_layers-1){
-   network->output_layer = curr ;
-   break; 
-   }else{
-   prev = curr ; 
-   curr = layer_create(); 
-   i++ ;
+     while(i < num_layers){	  
+      layer_init(curr,layer_outputs[i],prev);
+      prev->next = curr; 
+       if(i == num_layers-1){
+        network->output_layer = curr ;
+        break; 
+       }else{
+        prev = curr ; 
+        curr = layer_create(); 
+        i++ ;
    }  
   }
   return network;
 }
 
-/* Frees the space allocated to ann. */
+/* Frees the space allocated to ann. */ 
 void ann_free(ann_t *ann)
 {
-  /**** PART 2 - QUESTION 2 ****/
-
-  /* 2 MARKS */
+  layer_t* curr = ann->input_layer->next;  
+  while(curr->next != NULL){
+    layer_free(curr->prev);
+    curr = curr->next;  
+  }
+  free(ann);
 }
 
 /* Forward run of given ann with inputs. */
 void ann_predict(ann_t const *ann, double const *inputs)
 {
-  /**** PART 2 - QUESTION 3 ****/
-
-  /* 2 MARKS */
+ 
 }
 
 /* Trains the ann with single backprop update. */
