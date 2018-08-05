743a0177849690796ffa35eedf5d476a357d0b45
Maelhann Roze <ankou@roaming-54-ee-75-d1-cb-78.ic.ac.uk>
2018-08-05 15:48:32 -0500

    final version, ann learns to even all inputs to 1

--- ann.c
+++ ann.c
@@ -1,50 +1,59 @@
 #include "ann.h"
 
-/* Creates and returns a new artificial neural net */
+/* Creates and returns a new ann. */
 ann_t *ann_create(int num_layers, int *layer_outputs)
-{ 
-  ann_t *network = malloc(sizeof(ann_t)) ;
-  network->input_layer = layer_create() ;
-  layer_init(network->input_layer,layer_outputs[1],NULL) ; 
-  int i =  2 ;
-  layer_t* prev = network->input_layer ; 
-  layer_t* curr = layer_create() ; 
-     while(i < num_layers){	  
-      layer_init(curr,layer_outputs[i],prev);
-      prev->next = curr; 
-       if(i == num_layers-1){
-        network->output_layer = curr ;
-        break; 
-       }else{
-        prev = curr ; 
-        curr = layer_create(); 
-        i++ ;
-   }  
-  }
-  return network;
-}
-
-/* Frees the space allocated to ann. */ 
-void ann_free(ann_t *ann)
 {
-  layer_t* curr = ann->input_layer->next;  
-  while(curr->next != NULL){
-    layer_free(curr->prev);
-    curr = curr->next;  
-  }
-  free(ann);
+   ann_t* network = malloc(sizeof(ann_t));
+   if(network == NULL){
+   return NULL ; 
+   }
+   network->input_layer = layer_create() ;
+
+   layer_init(network->input_layer,layer_outputs[1],NULL); 
+   network->input_layer->num_inputs = layer_outputs[0];  
+   
+   layer_t* curr = layer_create() ;
+   layer_t* prev = network->input_layer;    
+
+   int i = 1 ; 
+   while(i < num_layers){
+     layer_init(curr,layer_outputs[i],prev); 
+     prev->next = curr ; 
+     curr->next = layer_create();
+     prev = curr;
+     curr = curr->next ; 
+     i++;     
+   } 
+  network->output_layer = prev ; 
+  return network ;
+
+ }
+
+/* Frees the space allocated to ann. */
+void ann_free(ann_t *ann)
+{ 
+   layer_t* curr = ann->input_layer->next ; 
+   while(curr->next != NULL){
+   layer_free(curr->prev); 
+   curr = curr->next;
+   } 
+   layer_free(curr); 
+   free(ann); 
 }
 
-/* Forward run of given a. neural net  with inputs. */
+/* Forward run of given ann with inputs. */
 void ann_predict(ann_t const *ann, double const *inputs)
 {
- ann->input_layer->outputs = inputs; 
- layer_t* curr = ann->input_layer->next ;  
- while(curr->next != NULL ){
-  layer_compute_outputs(curr);
-  curr = curr->next ; 
- }
- layer_compute_outputs(ann->output_layer); 
+  for(int i = 0 ; i < ann->input_layer->num_outputs ;i++){
+   ann->input_layer->outputs[i] = inputs[i]; 
+  }
+  layer_t* curr = ann->input_layer->next ; 
+  while(curr->next != NULL){
+   layer_compute_outputs(curr); 
+   curr = curr->next ; 
+  }
+  layer_compute_outputs(ann->output_layer);
+
 }
 
 /* Trains the ann with single backprop update. */
@@ -56,21 +65,22 @@ void ann_train(ann_t const *ann, double const *inputs, double const *targets, do
   assert(targets != NULL);
   assert(l_rate > 0);
 
-  /* Run forward pass. */
-  ann_predict(ann, inputs); 
-  for(int j = 0 ; j < ann->output_layer->num_inputs ; j++){
-   ann->output_layer->deltas[j] 
-	   = sigmoidprime(ann->output_layer->outputs[j])
-	   *(targets[j] - ann->output_layer->outputs[j]);
+  /* Run forward pass. */ 
+  ann_predict(ann, inputs);
+  for(int j = 0 ; j < ann->output_layer->num_outputs ;j++){
+  ann->output_layer->deltas[j] = sigmoidprime(ann->output_layer
+  ->outputs[j])	*(targets[j] - ann->output_layer->outputs[j]) ;
   }
-  layer_update(ann->output_layer,l_rate);
-  layer_t* curr = ann->output_layer->prev; 
+  
+  layer_t* curr = ann->output_layer->prev ; 
   while(curr->prev != NULL){
-    layer_compute_deltas(curr);
-    layer_update(curr,l_rate); 
-    curr = curr->prev; 
-  }
-  ann_predict(ann,inputs); 
-  
-  
+     layer_compute_deltas(curr); 
+     curr = curr->prev ;  
+  } 
+  curr = ann->input_layer->next; 
+  while(curr->next != NULL){
+   layer_update(curr,l_rate); 
+   curr = curr->next;
+  } 
+  layer_update(ann->output_layer,l_rate);  
 }
--- layer.c
+++ layer.c
@@ -3,7 +3,7 @@
 /* The sigmoid function and derivative. */
 double sigmoid(double x)
 {
-  return 1 / (1 + exp(-x)) ;  
+  return (1 / 1 + (1/exp(x))); 
 }
 
 double sigmoidprime(double x)
@@ -14,110 +14,131 @@ double sigmoidprime(double x)
 /* Creates a single layer. */
 layer_t *layer_create()
 {
-   layer_t* ptr = malloc(sizeof(layer_t)); 
-   ptr->outputs = NULL; 
-   ptr->prev = NULL; 
-   ptr->next = NULL; 
-   ptr->weights = NULL; 
-   ptr->biases = NULL; 
-   ptr->deltas = NULL;  
-   ptr->num_inputs = 0; 
-   ptr->num_outputs = 0;
+  layer_t* layer = malloc(sizeof(layer_t)); 
+  if(layer == NULL){
+   // if alloc fails 
+   return NULL ; 
+  }else{
+  //init all integer vals to zero and all pointers to NULL 
+  layer->num_outputs = 0 ; 
+  layer->num_inputs = 0 ;
+  
+  layer->outputs = NULL; 
+  layer->prev = NULL ; 
+  layer->next = NULL ; 
+  layer->weights = NULL ;
+  layer->biases = NULL ; 
+  layer->deltas = NULL ;
+
+  return layer ;
+  } 
 }
 
 /* Initialises the given layer. */
 bool layer_init(layer_t *layer, int num_outputs, layer_t *prev)
-{
-  
-  layer->num_outputs = num_outputs; 
-  layer->outputs = calloc(num_outputs,sizeof(double));	
-  for(int i = 0 ; i < num_outputs;i++){
-  layer->outputs[i] = 0 ; 
-  }
-  if(prev != NULL){
-  layer->prev = prev ;
-  layer->num_inputs = prev->num_outputs;  
-  layer->biases = calloc(prev->num_outputs,sizeof(double));
-  layer->deltas = calloc(prev->num_outputs,sizeof(double)); 
-  // initialize biases and deltas to zero. 
-  for(int j = 0; j < prev->num_outputs;j++){
-   layer->biases[j] = 0 ;
-   layer->deltas[j] = 0 ; 
+{ 
+  layer->num_outputs = num_outputs ; 
+  layer->outputs = malloc(num_outputs*sizeof(double)); 
+   if(layer->outputs != NULL){ 
+      for(int i = 0 ; i < num_outputs ; i++){
+      layer->outputs[i] = 0 ; 
+    }	
+   }else{
+    return true ; 
    }
-  layer->weights = calloc(prev->num_outputs,sizeof(double*)); 
-  for(int k = 0 ; k < prev->num_outputs ; k++){
-     layer->weights[k] = calloc(prev->num_outputs,sizeof(double)); 
-     for(int l = 0 ; l < prev->num_outputs; l++){
-       layer->weights[k][l] = ANN_RANDOM(); 
+
+   if(prev != NULL){
+   // if we are not dealing with input layer, then allocate arrays. 
+   layer->num_inputs = prev->num_outputs;
+   layer->prev = prev ;  
+   layer->biases = malloc(layer->num_inputs*sizeof(double)); 
+   layer->deltas = malloc(layer->num_inputs*sizeof(double)); 
+     if(layer->biases != NULL && layer->deltas != NULL){
+      for(int i = 0 ; i < layer->num_inputs ; i++){
+      	layer->biases[i] = 0 ; 
+        layer->deltas[i] = 0 ;   
+      }
+     }else{
+  	return true;
      }
+
+   layer->weights = malloc(layer->num_inputs * sizeof(double*));
+    if(layer->weights != NULL){
+     for(int i = 0 ; i < layer->num_inputs ; i++){
+       layer->weights[i] = malloc(layer->num_inputs*sizeof(double));
+       if(layer->weights[i] == NULL){
+ 	 return true ; 
+       }
+        for(int j = 0 ; j < layer->num_inputs ; j++){
+         layer->weights[i][j] = ANN_RANDOM();
+        } 
+     }
+    }else{
+     return true ; 
+    } 
+    
   }
-   if(layer->weights == NULL ||
-      layer->biases == NULL  ||
-      layer->deltas == NULL){
-   return true;
-    }
-  }
-  
-  if(layer->outputs == NULL){
-   return true ; 
-  }
- return false;  
+  return false; 
 }
 
 /* Frees a given layer. */
 void layer_free(layer_t *layer)
 {
-  free(layer->next); 
-  free(layer->prev); 
-  free(layer->outputs);
-  free(layer->biases);
-  free(layer->deltas); 
-  for(int i = 0 ; i < layer->num_inputs ;i++){
-    free(layer->weights[i]); 
-  }
-  free(layer->weights);  
-  free(layer);   
+ free(layer->outputs);
+ 
+ if(layer->prev != NULL){
+ free(layer->deltas);
+ free(layer->biases); 
+ 
+  for(int i = 0 ; i < layer->num_inputs ; i++){
+  free(layer->weights[i]); 
+  } 
+  
+  free(layer->weights); 
+  layer->prev = NULL ; 
+ }
+ 
+ free(layer);
 }
 
 /* Computes the outputs of the current layer. */
 void layer_compute_outputs(layer_t const *layer)
 {
-  double sum = 0 ;
-  for(int j = 0 ; j < layer->num_outputs ; j++){
-    sum = 0; 
-       for(int i = 0 ; i < layer->prev->num_outputs ; i++){
-     sum += layer->prev->outputs[i] * layer->weights[i][j]; 
-       }
-   layer->outputs[j] = sigmoid(layer->biases[j] + sum); 
-  }
-}
-
-/* Computes the delta errors for this layer
- * uses the back-propagation algorithm */
-void layer_compute_deltas(layer_t const *layer)
-{   assert(layer != NULL);
-    double sum = 0 ; 
-    for(int i = 0 ; i < layer->num_inputs; i++){
-     sum = 0 ; 
-      for(int j = 0 ;  j < layer->num_outputs; j++){
-  	 sum += layer->next->weights[i][j] * layer->next->deltas[j];     
+   double sum  ;
+   for(int j = 0 ; j < layer->num_outputs ; j++){
+     sum = 0 ;     
+      for(int i = 0 ; i < layer->prev->num_outputs ; i++){
+        sum += layer->weights[i][j] * layer->prev->outputs[i];    
       }
-     layer->deltas[i] = sigmoidprime(layer->outputs[i])*sum ;
-    }
+     layer->outputs[j] = sigmoid(layer->biases[j] + sum);  
+    } 
 }
 
-/* Updates weights and biases according
- * to the delta errors given learning rate. */
+/* Computes the delta errors for this layer. */
+void layer_compute_deltas(layer_t const *layer)
+{
+  double sum ; 
+  for(int i = 0 ; i < layer->num_inputs ; i++){
+    sum = 0; 
+     for(int j = 0 ; j < layer->next->num_inputs;j++){
+      sum += layer->next->weights[i][j] * layer->next->deltas[j] ;  
+     }
+    layer->deltas[i] = sigmoidprime(layer->outputs[i])
+			*sum ;   
+   }
+}
+
+/* Updates weights and biases according to the delta errors given learning rate. */
 void layer_update(layer_t const *layer, double l_rate)
-{ 
-  assert(layer != NULL);
+{
+  for(int j = 0 ; j < layer->num_inputs ; j++){
+    layer->biases[j] += l_rate * layer->deltas[j];
+  }
+
   for(int i = 0 ; i < layer->prev->num_inputs ; i++){
-    for(int j = 0 ; j < layer->num_inputs ; j++){
-    layer->weights[i][j] += l_rate *
-	    layer->prev->outputs[i] * layer->deltas[j]; 
-    layer->biases[j] += l_rate * layer->deltas[j];  
-		    
-    }    
-    
+   for(int j = 0 ; j < layer->num_inputs ; j++){
+    layer->weights[i][j] += l_rate 
+              * layer->prev->outputs[i] * layer->deltas[j];   
+    }
   }
 }
